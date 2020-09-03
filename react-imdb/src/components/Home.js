2b340264e32ea01998b135b4bf5290194b17d45d
arianrah <arianrahimi@icloud.com>
2020-09-02 21:50:23 -0700

    Completed hero image for home page. Hero image is the #1 featured movie from tmdb live.

--- react-imdb/src/components/Home.js
+++ react-imdb/src/components/Home.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { API_BASE_URL, POSTER_SIZE, BACKDROP_SIZE } from '../config';
+import {
+	API_BASE_URL,
+	POSTER_SIZE,
+	IMAGE_BASE_URL,
+	BACKDROP_SIZE
+} from '../config';
 
 // import Components
 import HeroImage from './elements/HeroImage';
@@ -17,9 +22,16 @@ const Home = () => {
 
 	console.log(state);
 
+	if (error) return <div>Something went wrong ...</div>;
+	if (!state.movies[0]) return <Spinner />;
+
 	return (
 		<>
-			<HeroImage />
+			<HeroImage
+				image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.heroImage.backdrop_path}`}
+				title={state.heroImage.original_title}
+				text={state.heroImage.overview}
+			/>
 			<SearchBar />
 			<Grid />
 			<MovieThumb />
