0e81e21be5d659967258fc507426dfe22590b1ae
arianrah <arianrahimi@icloud.com>
2020-09-29 16:24:31 -0700

    Added page link functionality for Logo->home and moviethumb->specified movie.

---
+++ react-imdb/src/components/NotFound.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const NotFound = () => <div>404izzle nothing found in hizzle.</div>
+
+export default NotFound
--- react-imdb/src/components/elements/Header.js
+++ react-imdb/src/components/elements/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from '@reach/router'
 
 import RMDBLogo from '../../images/reactMovie_logo.png';
 import TMDBLogo from '../../images/tmdb_logo.svg';
@@ -12,8 +13,10 @@ import {
 const Header = () => (
 	<StyledHeader>
 		<div className="header-content">
+			<Link to="/">
 			<StyledRMDBLogo src={RMDBLogo} alt="rmdb-logo" />
 			<StyledTMDBLogo src={TMDBLogo} alt="tmdb-logo" />
+			</Link>
 		</div>
 	</StyledHeader>
 );
